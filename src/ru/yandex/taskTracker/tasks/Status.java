81705c1cde5d9a423abee7313d4dbd00a1729fe9
Alexandr Antonov <antonovsacha@yandex.ru>
2022-11-22 00:00:22 +0300

    fix: убран переопределенный метод toString()

--- src/ru/yandex/taskTracker/tasks/Status.java
+++ src/ru/yandex/taskTracker/tasks/Status.java
@@ -1,13 +1,8 @@
 package ru.yandex.taskTracker.tasks;
 
 public enum Status {
-    //ПРОЕКТ СЫРОЙ, Можешь в принципе глянуть, я вроде только недопонял про Manager, мне надо дженерик создать что ли
     NEW,
     IN_PROGRESS,
-    DONE;
-
-    @Override
-    public String toString() {
-        return name().charAt(0) + name().substring(1).toLowerCase();
-    }
+    DONE
+    //убрал оверрайд метода toString()
 }
