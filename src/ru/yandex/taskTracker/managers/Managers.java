2cb8bfb06424af97d5e518f1b04278503b87d91f
Alexandr Antonov <antonovsacha@yandex.ru>
2022-12-20 22:19:12 +0300

    refactor : изменили параметр с дженерика на обычный

--- src/ru/yandex/taskTracker/managers/Managers.java
+++ src/ru/yandex/taskTracker/managers/Managers.java
@@ -12,6 +12,6 @@ public class Managers {
     }
 
     static public HistoryManager getHistoryManager() {
-        return new InMemoryHistoryManager<>();
+        return new InMemoryHistoryManager();
     }
 }
