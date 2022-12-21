185235a95e838754aea7a8d6ef9d240ef4808617
Alexandr Antonov <antonovsacha@yandex.ru>
2022-12-21 21:27:50 +0300

    refactor/bugfix : параметризовали CustomLinkedList, добавили модификаторы доступа,

--- src/ru/yandex/taskTracker/managers/historyManager/InMemoryHistoryManager.java
+++ src/ru/yandex/taskTracker/managers/historyManager/InMemoryHistoryManager.java
@@ -8,19 +8,13 @@ import java.util.List;
 
 public class InMemoryHistoryManager implements HistoryManager {
 
-    // Сидишь такой, попиваешь сок, купаешься на пляже, катаешься на байке...
-    // А потом просыпаешься и понимаешь, что ты не Александр Валюк
-    // Как дела? Что нового? Фотки в инсте топ!
-    private final CustomLinkedList customLinkedList = new CustomLinkedList();
+    private final CustomLinkedList<Task> customLinkedList = new CustomLinkedList<>();
 
+    //ОЧЕНЬ РАД ЗА ТЕБЯ!!! Красавчик! Горы и море - это классно, смотри не заболей, раз море холодное
+    //Заранее с внж не буду позравлять, шоб не сглазить как говорится
     @Override
     public void addHistory(Task task) {
-        customLinkedList.addElement(task);
-        //Код с прошлого ТЗ
-        //if (historyList.size() == 10) {
-        //    historyList.remove(0);
-        //}
-        //historyList.add(task);
+        customLinkedList.addElement(task, task.getId());
     }
 
     @Override
@@ -36,35 +30,39 @@ public class InMemoryHistoryManager implements HistoryManager {
 
 }
 
-class CustomLinkedList {
-    private final HashMap<Integer, CustomLinkedList.Node> historyMap = new HashMap<>();
-    private Node head = null;
-    private Node tail = null;
+// Вообще изначально сделал вложенный класс, потом думаю, а нафиг надо, вынес его в отдельный
+// не ставил модификаторы доступа, т.к. подумал, что КастомЛист используется только в этом пакете и на кой черт ему модификатор
+// public, пусть лучше побудет package-private.
+// Если надо..могу перенести в отдельный файл, просто отталкивался от места, где он используется. А используется он только
+// тут, поэтому чтобы разрабу не лазить от одного файла к другому. В общем, как скажешь
+class CustomLinkedList<T> {
+
+    private final HashMap<Integer, Node<T>> historyMap = new HashMap<>();
+    private Node<T> head = null;
+    private Node<T> tail = null;
     int size = 0;
 
-    public ArrayList<Task> getTaskList() {
-        ArrayList<Task> historyList = new ArrayList<>();
-        Node currentNode = head;
+    public ArrayList<T> getTaskList() {
+
+        ArrayList<T> historyList = new ArrayList<>();
+        Node<T> currentNode = head;
         while (currentNode != null) {
             historyList.add(currentNode.data);
             currentNode = currentNode.next;
         }
         return historyList;
     }
-    // Что самое ужасное, в вашей работе?
-    // - нуу, когда дети шумят, - ответил учитель
-    // - нет, самое ужасное, когда нужно сдвинуть кнопку на пиксель влево, - ответил ux-ui дизайнер
-    // С черными глазами и дрожашим голосом отвечает джавист - "Когда дебажишь два с половиной часа и не находишь ошибку.
-    // А оказывается, что нужно было написать вызов команды на строчку позже..." Все сидели молча..
+    //Я ТАК ОРАЛ С ТОГО, ЧТО ГЛАВНАЯ ПРОБЛЕМА СИДИТ ПЕРЕД МОНИТОРОМ
+    //ПРОСТО АХАХАХ
 
-    void addElement(Task task) {
-        if (historyMap.containsKey(task.getId())) {
-            removeElement(task.getId());
+    public void addElement(T element, Integer id) {
+        if (historyMap.containsKey(id)) {
+            removeElement(id);
         }
-        link(task);
+        link(element, id);
     }
 
-    void removeElement(int id) {
+    public void removeElement(int id) {
         if (historyMap.containsKey(id)) {
             unlink(historyMap.get(id));
             historyMap.remove(id);
@@ -75,37 +73,39 @@ class CustomLinkedList {
         }
     }
 
-    private void link(Task task) {
-        Node node = new Node(task);
+    private void link(T element, Integer id) {
+        Node<T> node = new Node<>(element);
         if (head == null && tail == null) {
             head = node;
             tail = node;
-            historyMap.put(task.getId(), node);
+            historyMap.put(id, node);
         } else {
-            linkLast(task);
+            linkLast(element, id);
         }
         size++;
 
     }
 
-    private void linkLast(Task task) {
-        Node newTail = new Node(task);
-        Node oldTail = tail;
+    private void linkLast(T element, Integer id) {
+        Node<T> newTail = new Node<>(element);
+        Node<T> oldTail = tail;
         oldTail.next = newTail;
         newTail.prev = oldTail;
         tail = newTail;
-        historyMap.put(task.getId(), newTail);
+        historyMap.put(id, newTail);
     }
 
-    private void unlink(Node nodeToRemove) {
-        final Node prev = nodeToRemove.prev;
-        final Node next = nodeToRemove.next;
+    private void unlink(Node<T> nodeToRemove) {
+        final Node<T> prev = nodeToRemove.prev;
+        final Node<T> next = nodeToRemove.next;
 
         if (prev == null) {
             head = next;
         } else {
             prev.next = next;
             nodeToRemove.prev = null;
+            // А-нет)) тут бы с тобой поспорили разрабы LinkedList, т.к я свято спер оттуда этот метод
+            // + все же я считаю, что это правильно, т.к GC явно понимает, что нужно удалить объект
         }
 
         if (next == null) {
@@ -118,13 +118,13 @@ class CustomLinkedList {
         nodeToRemove.data = null;
     }
 
-    //Вижу фронтендера - сразу хейчу, даже не спрашиваю его
-    private class Node {
-        private Task data;
-        private Node prev;
-        private Node next;
+    // .net - это педики, которые сперли все с java, потом их прижали и они сделали свое говно
+    private static class Node<E> {
+        private E data;
+        private Node<E> prev;
+        private Node<E> next;
 
-        public Node(Task data) {
+        public Node(E data) {
             this.data = data;
             this.next = null;
             this.prev = null;
