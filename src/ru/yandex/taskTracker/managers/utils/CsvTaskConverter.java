3faeaaae064d3b69794d79f1cdf19f0126ca4716
Alexandr Antonov <antonovsacha@yandex.ru>
2023-02-01 19:01:58 +0300

    feature: добавлен функционал со временем, записывающий данные о времени в файл

--- src/ru/yandex/taskTracker/managers/utils/CsvTaskConverter.java
+++ src/ru/yandex/taskTracker/managers/utils/CsvTaskConverter.java
@@ -8,6 +8,7 @@ import ru.yandex.taskTracker.tasks.Status;
 import ru.yandex.taskTracker.tasks.Subtask;
 import ru.yandex.taskTracker.tasks.Task;
 
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
@@ -17,7 +18,7 @@ public class CsvTaskConverter {
     public static ArrayList<String> getDataAsStringList(Map<Integer, Task> tasks, Map<Integer, Epic> epics, Map<Integer,
             Subtask> subtasks, HistoryManager history) {
         ArrayList<String> dataList = new ArrayList<>();
-        String title = "id,Type,Name,Status,Description,Epic";
+        String title = "id,Type,Name,Status,Description,Epic,Duration,StartTime";
         dataList.add(title);
         dataList.addAll(getTasksAsStringList(tasks));
         dataList.addAll(getEpicsAsStringList(epics));
@@ -66,34 +67,51 @@ public class CsvTaskConverter {
 
     private static String toString(Subtask subtask){
         return subtask.getId() + "," + TaskType.SUBTASK + "," + subtask.getName() + ","
-                + subtask.getStatus() + "," + subtask.getDescription() + "," + subtask.getEpicID();
+                + subtask.getStatus() + "," + subtask.getDescription() + "," + subtask.getEpicID()+ "," + subtask.getDuration()
+                + "," + subtask.getStartTime();
     }
 
     private static String toString(Epic epic){
         return epic.getId() + "," + TaskType.EPIC + "," + epic.getName() + ","
-                + epic.getStatus() + "," + epic.getDescription() + ",";
+                + epic.getStatus() + "," + epic.getDescription() + ","+ "отсутствует," + + epic.getDuration() + "," + epic.getStartTime();
     }
 
     private static String toString(Task task){
         return task.getId() + "," + TaskType.TASK + "," + task.getName() + ","
-                + task.getStatus() + "," + task.getDescription() + ",";
+                + task.getStatus() + "," + task.getDescription() + "," + "отсутствует," + task.getDuration() + "," + task.getStartTime();
     }
 
     public static Task taskFromString(String list) {
         String[] stringData = list.split(",");
-        if (stringData.length > 7) {
+        if (stringData.length > 9) {
             throw new ManagerSaveException("Неверное оформление файла: Указано большое количество запятых");
         }
         int id = Integer.parseInt(stringData[0]);
         String name = stringData[2];
         Status status = getTaskStatus(stringData[3]);
         String description = stringData[4];
-
+        long duration;
+        LocalDateTime startTime;
         if (TaskType.TASK.toString().equals(stringData[1])) {
+            if (isStartTimeAndDurationExist(stringData[6], stringData[7])){
+                duration = Long.parseLong(stringData[6]);
+                startTime = LocalDateTime.parse(stringData[7]);
+                return new Task(name, description, status, id, duration, startTime);
+            }
             return new Task(name, description, status, id);
         } else if (TaskType.EPIC.toString().equals(stringData[1])) {
+            if (isStartTimeAndDurationExist(stringData[6], stringData[7])){
+                duration = Long.parseLong(stringData[6]);
+                startTime = LocalDateTime.parse(stringData[7]);
+                return new Epic(name, description, status, id, duration, startTime);
+            }
             return new Epic(name, description, status, id);
         } else if (TaskType.SUBTASK.toString().equals(stringData[1])) {
+            if (isStartTimeAndDurationExist(stringData[6], stringData[7])){
+                duration = Long.parseLong(stringData[6]);
+                startTime = LocalDateTime.parse(stringData[7]);
+                return new Subtask(name, description, status, id, Integer.parseInt(stringData[5]), duration, startTime);
+            }
             return new Subtask(name, description, status, id, Integer.parseInt(stringData[5]));
         } else {
             throw new ManagerSaveException("неверный тип таска в файле");
@@ -108,6 +126,13 @@ public class CsvTaskConverter {
         }
         throw new ManagerSaveException("Невозможно сравнить Enum статус");
     }
+    private static boolean isStartTimeAndDurationExist(String duration, String startTime){
+        if(duration.equals("0") || startTime.equals("null")){
+            return false;
+        } else {
+            return true;
+        }
+    }
 
     public static List<Integer> historyFromString(String value) {
         List<Integer> intValuesList = new ArrayList<>();
@@ -118,4 +143,5 @@ public class CsvTaskConverter {
         }
         return intValuesList;
     }
+
 }
