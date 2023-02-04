05e24f08c1846def1a109600462b05846ff670fb
Alexandr Antonov <antonovsacha@yandex.ru>
2023-02-05 00:27:02 +0300

    feature: переделан метод getDefault(), добавлены методы создания FileBackedTaskManager, InMemoryTaskManager

--- src/ru/yandex/taskTracker/managers/Managers.java
+++ src/ru/yandex/taskTracker/managers/Managers.java
@@ -2,16 +2,30 @@ package ru.yandex.taskTracker.managers;
 
 import ru.yandex.taskTracker.managers.historyManager.InMemoryHistoryManager;
 import ru.yandex.taskTracker.managers.historyManager.HistoryManager;
+import ru.yandex.taskTracker.managers.httpServer.HttpTaskManager;
 import ru.yandex.taskTracker.managers.taskManager.TaskManager;
 import ru.yandex.taskTracker.managers.taskManager.InMemoryTaskManager;
+import ru.yandex.taskTracker.managers.taskManager.fileManager.FileBackedTaskManager;
+
+import java.nio.file.Path;
 
 public class Managers {
 
-    static public TaskManager getDefault() {
-        return new InMemoryTaskManager();
+    static public TaskManager getDefault(String URL) {
+        return new HttpTaskManager(URL);
     }
 
     static public HistoryManager getHistoryManager() {
         return new InMemoryHistoryManager();
     }
+
+    static FileBackedTaskManager getFileBackedManager(String path){
+        return FileBackedTaskManager.loadFromFile(Path.of(path));
+    }
+
+    static InMemoryTaskManager getInMemoryTaskManager() {
+        return new InMemoryTaskManager();
+    }
+
+
 }
